3d0c0f8475d37b7c83dea82e0a34a0b11e35cf24
Ceressia  Gourdet <ceressiarg@gmail.com>
2026-04-07 08:00:34 -0500

    ajout du modèle Livre

--- bibliotheque/livres/models.py
+++ bibliotheque/livres/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+GENRES_CHOIX = [
+    ('aventure', 'Aventure'),
+    ('biographie', 'Biographie'),
+    ('fantastique', 'Fantastique'),
+    ('histoire', 'Histoire'),
+    ('horreur', 'Horreur'),
+    ('policier', 'Policier'),
+    ('roman', 'Roman'),
+    ('science_fiction', 'Science-Fiction'),
+    ('thriller', 'Thriller'),
+]
+
+class Livre(models.Model):
+    titre = models.CharField(max_length=100)
+    auteur = models.CharField(max_length=50)
+    genre = models.TextField(max_length=50, choices=GENRES_CHOIX)
+    description = models.CharField(max_length=200)
+    date_de_publication = models.DateField()
+    proprietaire = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.titre} - {self.auteur}"
